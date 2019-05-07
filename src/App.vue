3c468512e130a256561eaf49fc00b86a03ea9af8
Alejandro Guerrero <gbrayhan@gmail.com>
2019-05-07 17:39:55 -0500

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template lang="pug">
   #app
-    p(v-show="showValue") {{ value }}
-    p(v-if="showValue") {{ value }}
-    p(v-else-if="condicion") {{ "recurso alterno" }}
-    p(v-else) {{ 'recurso no disponible' }}
-    ul
-      li(v-for="item in items") {{ item * 2 }}
     input(v-model="name")
-    p {{ name }}
-    a( v-bind:href="url" ) direccion web
+    input(v-model="lastName")
+
+    p {{ fullname }}
 </template>
 <script>
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Mundo vuejs',
-      person: {
-        name: 'Alex'
-      },
-      condicion: true,
-      showValue: false,
-      value: 'Elemento a Mostrar',
-      items: [1, 2, 3, 4, 5, 6, 7, 5, 4, 3, 2, 3],
       name: '',
-      url: 'https://www.google.com'
+      lastName: ''
+    }
+  },
+  computed: {
+    fullname () {
+      // return this.name + '' + this.lastName
+      return `${this.name} ${this.lastName}`
     }
   }
 }
